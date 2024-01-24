7014509c93770427219704d829142f72165ef410
yudhistira arya <aryarajasa0@gmail.com>
2024-01-24 16:39:32 +0800

    scanner

--- src/Screens/Locations/Scanner.tsx
+++ src/Screens/Locations/Scanner.tsx
@@ -1,4 +1,4 @@
-import React, {useState, useEffect} from 'react';
+import React, {useState, useEffect, useRef} from 'react';
 import {
   ScrollView,
   StatusBar,
@@ -9,15 +9,14 @@ import {
   Text,
   ActivityIndicator,
 } from 'react-native';
-import {IconButton} from 'react-native-paper';
+import {Chip, IconButton} from 'react-native-paper';
 import {COLORS} from '~/Constant/Color';
 import {useNavigation} from '@react-navigation/native';
 import {Request} from '~/Utils';
 import AsyncStorage from '@react-native-async-storage/async-storage';
-import {
-  AutocompleteDropdownContextProvider,
-  AutocompleteDropdown,
-} from 'react-native-autocomplete-dropdown';
+import {Card} from 'react-native-paper';
+import {AutocompleteDropdown} from 'react-native-autocomplete-dropdown';
+import {isNull, defaultTo} from 'lodash';
 
 function Scanner(p) {
   const navigation = useNavigation();
@@ -29,9 +28,9 @@ function Scanner(p) {
   const [search, setSearch] = useState<any>('');
   const [timeoutId, setTimeoutId] = useState<number | null>(null);
   const [dataSelected, setDataSelected] = useState<object | null>();
+  const autocompleteRef = useRef(null);
 
   useEffect(() => {
-    console.log('asdfflkl');
     const fetchData = async () => {
       try {
         const storedToken = await AsyncStorage.getItem(
@@ -65,16 +64,16 @@ function Scanner(p) {
   const onSuccess = (res: any) => {
     if (res.data) {
       let newData = res.data.map(set => ({...set, title: set.code}));
-
-      if (res.data.length === 1 && res.data[0].code === search) {
-        setSelectedItem({...res.data[0], title: res.data[0].code});
+      if (res.data.length == 1 ){ 
+        if(res.data[0].code === search) {
+        setSelectedItem(null);
         handleDelayedFunction({...res.data[0], title: res.data[0].code});
+      }}else{
+        setDataSelected(null);
       }
-
       setlocations(newData);
     }
   };
-
   const onFailed = (error: any) => {
     console.error('Failed to fetch data:', error);
   };
@@ -83,10 +82,12 @@ function Scanner(p) {
     setSearch(value);
   };
 
-
   const onSelected = value => {
-    setSelectedItem(value);
+    setSelectedItem(null);
     handleDelayedFunction(value);
+    if (autocompleteRef.current) {
+      autocompleteRef.current.focus();
+    }
   };
 
   const handleDelayedFunction = value => {
@@ -107,10 +108,13 @@ function Scanner(p) {
   const onSuccessGetDetail = res => {
     setDataSelected(res);
     setLoading(false);
+    if (autocompleteRef.current) {
+      autocompleteRef.current.clear();
+      autocompleteRef.current.focus();
+    }
   };
 
   const onFailedGetDetail = res => {
-    console.log(res);
     setLoading(false);
   };
 
@@ -130,14 +134,16 @@ function Scanner(p) {
           <View style={styles.container}>
             <View style={styles.section}>
               <AutocompleteDropdown
+                ref={autocompleteRef}
                 dataSet={locations}
                 closeOnBlur={true}
                 useFilter={true}
                 clearOnFocus={true}
                 textInputProps={{
                   placeholder: 'Start typing est...',
+                  autoFocus: true, // Add this line to set autofocus to true
                 }}
-                debounce={1000}
+                debounce={500}
                 onSelectItem={onSelected}
                 loading={loading}
                 onChangeText={getSuggestions}
@@ -145,40 +151,111 @@ function Scanner(p) {
                   color: '#8f3c96',
                 }}
               />
+
+              {!loading ? (
+                dataSelected ? (
+                  <ScrollView>
+                    <Card style={styles.card}>
+                      <Card.Content>
+                        <View style={styles.cardContent}>
+                          <View style={styles.info}>
+                            <Text style={styles.name}>{dataSelected.code}</Text>
+                            <View style={styles.rowContainer}>
+                              <Chip
+                                style={{
+                                  marginHorizontal: 4,
+                                  backgroundColor:
+                                    dataSelected.status != 'operational'
+                                      ? 'red'
+                                      : '#87D068',
+                                }}>
+                                <Text style={styles.price}>
+                                  {dataSelected.status}
+                                </Text>
+                              </Chip>
+                              {dataSelected.is_empty && (
+                                <Chip
+                                  style={{
+                                    marginHorizontal: 4,
+                                    backgroundColor: 'purple',
+                                  }}>
+                                  <Text style={styles.price}>Empty</Text>
+                                </Chip>
+                              )}
+                            </View>
+
+                            <View style={styles.descriptionContainer}>
+                              <View style={styles.descriptionRow}>
+                                <Text style={styles.descriptionTitle}>
+                                  Created At:
+                                </Text>
+                                <Text style={styles.descriptionText}>
+                                  {dayjs(dataSelected.created_at).format(
+                                    'DD/MM/YY',
+                                  )}
+                                </Text>
+                              </View>
+                              <View style={styles.descriptionRow}>
+                                <Text style={styles.descriptionTitle}>
+                                  Updated At:
+                                </Text>
+                                <Text style={styles.descriptionText}>
+                                  {dayjs(dataSelected.updated_at).format(
+                                    'DD/MM/YY',
+                                  )}
+                                </Text>
+                              </View>
+                              <View style={styles.descriptionRow}>
+                                <Text style={styles.descriptionTitle}>
+                                  Audited At:
+                                </Text>
+                                <Text style={styles.descriptionText}>
+                                  {dataSelected.audited_at &&
+                                  !isNull(dataSelected.audited_at)
+                                    ? dayjs(dataSelected.audited_at).format(
+                                        'DD/MM/YY',
+                                      )
+                                    : '-'}
+                                </Text>
+                              </View>
+                              <View style={styles.descriptionRow}>
+                                <Text style={styles.descriptionTitle}>
+                                  Stock Value:
+                                </Text>
+                                <Text style={styles.descriptionText}>
+                                  {dataSelected.stock_value}
+                                </Text>
+                              </View>
+                              <View style={styles.descriptionRow}>
+                                <Text style={styles.descriptionTitle}>
+                                  Max Volume:
+                                </Text>
+                                <Text style={styles.descriptionText}>
+                                  {defaultTo(dataSelected.max_volume, '-')}
+                                </Text>
+                              </View>
+                              <View style={styles.descriptionRow}>
+                                <Text style={styles.descriptionTitle}>
+                                  Max Weight:
+                                </Text>
+                                <Text style={styles.descriptionText}>
+                                  {defaultTo(dataSelected.max_weight, '-')}
+                                </Text>
+                              </View>
+                            </View>
+                          </View>
+                        </View>
+                      </Card.Content>
+                    </Card>
+                  </ScrollView>
+                ) : null
+              ) : (
+                <ActivityIndicator size="large" />
+              )}
             </View>
           </View>
         </ScrollView>
       </KeyboardAvoidingView>
-      {!loading ? (
-        dataSelected ? (
-          <ScrollView>
-            <View>
-              <View style={styles.container}>
-                <View style={styles.info}>
-                  <Text style={styles.name}>{dataSelected.code}</Text>
-                  <Text style={styles.price}>{dataSelected.status}</Text>
-                  <View style={styles.descriptionContainer}>
-                    <View style={styles.descriptionRow}>
-                      <Text style={styles.descriptionTitle}>Created At:</Text>
-                      <Text style={styles.descriptionText}>
-                        {dayjs(dataSelected.created_at).format('DD MM YYYY')}
-                      </Text>
-                    </View>
-                    <View style={styles.descriptionRow}>
-                      <Text style={styles.descriptionTitle}>Stock Value:</Text>
-                      <Text style={styles.descriptionText}>
-                        {dataSelected.stock_value}
-                      </Text>
-                    </View>
-                  </View>
-                </View>
-              </View>
-            </View>
-          </ScrollView>
-        ) : null
-      ) : (
-        <ActivityIndicator size="large" />
-      )}
     </ScrollView>
   );
 }
@@ -215,14 +292,13 @@ const styles = StyleSheet.create({
     marginBottom: 10,
   },
   price: {
-    fontSize: 18,
-    color: '#999',
-    marginBottom: 20,
+    fontSize: 12,
+    color: '#fefefe',
   },
   descriptionContainer: {
-    marginTop: 3,
-    borderTopWidth: 1, // Add top border to the first row
-    borderColor: '#ccc', // Border color
+    marginTop: 10,
+    borderTopWidth: 1,
+    borderColor: '#ccc',
   },
 
   descriptionRow: {
@@ -230,11 +306,10 @@ const styles = StyleSheet.create({
     justifyContent: 'space-between',
     alignItems: 'center',
     paddingVertical: 8,
-    borderBottomWidth: 1, // Add bottom border to each row
-    borderColor: '#ccc', // Border color
+    borderBottomWidth: 1,
+    borderColor: '#ccc',
   },
 
-  // Adjusted the fontSize, color, and added margin
   descriptionTitle: {
     fontSize: 16,
     fontWeight: 'bold',
@@ -242,11 +317,22 @@ const styles = StyleSheet.create({
     marginBottom: 5,
   },
 
-  // Adjusted the fontSize, color, and added margin
   descriptionText: {
     fontSize: 16,
     color: '#555',
     marginBottom: 5,
+  },
+  card: {
+    margin: 10,
+    borderRadius: 10,
+    elevation: 3,
+  },
+  cardContent: {
+    padding: 1,
+  },
+  rowContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
   },
 });
 
